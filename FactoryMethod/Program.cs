a30abd2fd59ccad14b186da588f7fe111bbf3182
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-13 10:48:31 +0300

    Factory Method Design Pattern
    First

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory2());
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    //Creating Factory
+    public class LoggerFactory : IloggerFactory
+    {
+        public Ilogger CreateLogger()
+        {
+            //Bussiness to decide factory   
+            return new MustafaAlparlanPamukLogger();
+        }
+    }
+
+    public class LoggerFactory2 : IloggerFactory
+    {
+        public Ilogger CreateLogger()
+        {
+            //Bussiness to decide factory   
+            return new MustafaPamukLogger();
+        }
+    }
+}
