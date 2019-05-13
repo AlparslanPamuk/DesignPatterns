232b0d28ec0ca3646308c702f112c08c6f984396
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-13 11:06:34 +0300

    Abstract Factory Design Pattern
    First

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+    }
+
+    public class Log4NetLogger:Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with log4Net ");
+        }
+    }
+
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with nLogger ");
+        }
+    }
+
+}
