65a2206bf8c68a6b3b270a74f1fa3cdfe9d580f8
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-15 11:23:23 +0300

    Prototype
    First

---
+++ Prototype/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { FirstName = "Mustafa", LastName = "Pamuk", City = "Istanbul", Id = 1 };
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Alparslan";
+
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine();
+
+            //Customer 1 and customer 2 refers to different variables.
+
+
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
