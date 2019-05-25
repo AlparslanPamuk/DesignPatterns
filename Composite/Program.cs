6c3fc014155d659ceacdc6e64c80e1e5bdc5aa3b
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-25 23:59:34 +0300

    Composite Design Pattern
    Second

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,8 +22,32 @@ namespace Composite
         string Name { get; set; }
     }
 
-    class Employee : IPerson
+    class Employee : IPerson,IEnumerable<IPerson>
     {
+        List<IPerson> _subortinates = new List<IPerson>();
+
+        public void AddSubordinate(IPerson person)
+        {
+            _subortinates.Add(person);
+        }
+
+        public void RemoveSubordinate(IPerson person)
+        {
+            _subortinates.Remove(person);
+        }
+
         public string Name { get; set; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
+
+
 }
