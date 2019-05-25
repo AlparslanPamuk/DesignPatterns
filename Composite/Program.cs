d06cbae7bfb1510c66a52be3c969cc36bfc80407
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-05-25 23:44:30 +0300

    Composite Design Pattern
    First

---
+++ Composite/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    // bir kurumdaki çalışanlar ve o çalışanların hiyerarşisini modelleyeceğiz
+    //çnümüzde bir ekran var bu ekranda kullanıcıları sürükle bırak yöntemiyle hiyerarşik bir yöntem çiziyoruz 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+     
+    interface IPerson // Kurum çalışanları 
+    {
+        string Name { get; set; }
+    }
+
+    class Employee : IPerson
+    {
+        public string Name { get; set; }
+    }
+}
