fdbb98a47d521d7e9940eea9ef84e785ff1fee4d
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-06 23:23:16 +0300

    OOP

---
+++ OOP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class human
+    {
+        public string name;
+        private int weight;
+        public void setWeight(int weight)
+        {
+            this.weight = weight;
+        }
+        public int getWeight()
+        {
+            return weight;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            human Alparslan = new human();
+            Alparslan.name = "Mustafa";
+            Alparslan.setWeight(80);
+            human Mustafa = new human();
+
+            Console.WriteLine("Alparslan's weight:" + Alparslan.getWeight());
+            Console.ReadLine();
+        }
+    }
+}
