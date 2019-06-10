22e608f9000527a10cf45b9a058570ed40cc12a8
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-10 17:26:19 +0300

    Proxy Design Pattern
    Second

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Proxy
@@ -10,13 +11,32 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
+            CreditManager manager = new CreditManager();
+            Console.WriteLine(manager.Calculate());
+            Console.WriteLine(manager.Calculate());
 
+            Console.ReadLine();
         }
     }
 
     abstract class CreditBase // Operation class
     {
         public abstract int Calculate();
-    }   
-       
+    }
+
+    class CreditManager : CreditBase
+    {
+        public override int Calculate()
+        {
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= i;
+                Thread.Sleep(1000);
+            }
+
+            return result;
+        }
+    }
+
 }
