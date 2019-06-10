d8b9749cd6c1f9914a6db5c15edac7051d807b24
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-10 17:45:13 +0300

    Decorator Design Pattern
    First

---
+++ Decorator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program // araç kiralama sistemi / normal fiyatlar mevcut / bazı dönemlerde bazı kullanıcılar için özel uygulama indirim
+                  // Araçla ilgili bazı özellikleri farklı müşteriler için farklı imkanlar sunmak istiyoruz.
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+    abstract class CarBase
+    {
+        public abstract string Make { get; set; }
+        public abstract string Model { get; set; }
+        public abstract string HirePrice { get; set; }
+    }
+}
