b4324cd4fc401f98d8d5657a0bc7b0ab3a7b6484
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-11 18:22:18 +0300

    Bridge Design Pattern
    Second

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -10,6 +10,39 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    abstract class MessageManagerBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message is saved");
+        }
+
+        public abstract void Send(Body body);
+    }
+
+    class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    class SmsSender : MessageManagerBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via SmsSender",body);
+        }
+    }
+
+    class EmailSender : MessageManagerBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via EmailSender", body);
         }
     }
 }
