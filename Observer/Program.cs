e99ff92d72ba9e1bbecede8b1faf057c3e9e981d
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-13 13:45:32 +0300

    Observer Design Pattern
    Second

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Observer
 {
-    class Program
+    class Program //Bir alışveriş sisteminde ürünün fiyatı düştüğünde beni haberdar et 
+        // haberdar edilmesi gereken müşteriler bu desenle haberdar edilebilir.
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    class ProductManager
+    {
+        List<Observer> _observers = new List<Observer>();
+        public void UpdatePrice()
+        {
+            Console.WriteLine("Product Price is Updated!");
+        }
+
+        
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update(); // standard method signature
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to Customer: Product Price is Changed");
+        }
+    }
+
+    class EmployeeObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("Message to Employee: Product Price is Changed");
         }
     }
 }
