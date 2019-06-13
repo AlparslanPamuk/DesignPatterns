37785f1b2b8020077f4101c634b0b894adeab290
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-13 13:32:19 +0300

    Strategy Design Pattern
    Second

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace Strategy
 {
-    class Program
+    class Program // bir bankanın farklı müşteri türlerine göre farklı kredi hesaplama 
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using before 2010");
+        }
+    }
+
+    class After2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using after 2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager bussiness");
         }
     }
 }
