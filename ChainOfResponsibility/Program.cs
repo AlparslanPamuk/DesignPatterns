fde3f7e59291b97fc4f0f38d612d6543447cf045
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-06-15 18:04:28 +0300

    Chain Of Responsibility Design Pattern
    First

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// bir şirketteyiz ve harcamalar yapıyoruz. Eğer harcamalar 100 tl nin altındaysa, müdür harcamaya yön verebiliyor.
+//eğer 1000 tl ise başkan yardımcısı onay verebiliyor 
+// 1000 tl üzerindeki harcamalara da başkan onay verebiliyor. 
+//harcama müdüre gider. 5000 tl ise başkana kadar kademelerden geçer 
+//hiyerarşik düzen oluşturma deseni. Sorumluluk zinciri tasarım deseni.
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
