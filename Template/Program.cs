db82b023d657c6b21a95c26c0111c5d7efbb396a
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-07-15 16:45:33 +0300

    Template Design Pattern
    Second

--- Template/Program.cs
+++ Template/Program.cs
@@ -8,8 +8,26 @@ namespace Template
 {
     class Program
     {
+        //bir method içerisinde farklı operasyonlarımız olduğunu düşün. Bunun bir iş katmanında iş sürecini kontrol altına aldığımız bir süreç.
+        //3 farklı işlem var ve biz bunları farklı mehtodlarla çağırırız ve bu methodun içindeki 3 işlemin de farklı durumlara göre farklı davrandığını düşün.
+        //
+        // Instead of many "if's" we use this way
+        //
+
         static void Main(string[] args)
         {
+            
+        }
+
+        abstract class ScoringAlgorithm                    //   bir oyunda erkek kadın ve çpocuk hesabı olacak, fakat hesapların farklılık gösterdiğini göreceğiz.
+        {
+            public void GenerateScore(int hits, TimeSpan time)    // My template method
+            {
+                int score = CalculateBaseScore(hits); // kadın erkek çocuk ayrımı yapmadan herkes için aynı puan alma hesaplaması.
+            }
+
+            public abstract int CalculateBaseScore(int hits);
+            
         }
     }
 }
