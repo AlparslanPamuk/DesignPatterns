14bac4f266816fd691c0f71ba9290959798a5fb1
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-07-19 15:40:05 +0300

    State Design Pattern
    Second

--- State/Program.cs
+++ State/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace State
 {
-    class Program
+    class Program // bir nesnenin ya da bir durumun mevcut durumunu kontrol etmek için kullanılır.
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    interface IState
+    {
+        void DoAction(Context context);
+    }
+
+    class ModifiedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Modified");
+        }
+    }
+
+    class DeleteState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Deleted");
+        }
+    }
+
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Added");
+        }
+    }
+
+    class Context
+    {
+        private IState _state;
+
+        public void SetState(IState state)
+        {
+            _state = state;                            // context'in statesini set etmiş oluyoruz.
+        }
+
+        public IState GetState()
+        {
+            return _state;
         }
     }
 }
