f9c8af7b64e5adb4b3dfba2fcf31e5d5e9637642
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-07-20 19:53:33 +0300

    Mediator Design Pattern
    Second

--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Mediator
 {
-    class Program
+    class Program // Ara bulucu yani farklı desenleri birbiriyle görüştürme
+        //  aynı piste inmeye çalışan 5 uçak bilgiyi kuleye gönderir, kule bunları gönderir hangi mesajı ne zaman kime göndereceğine karar verir.
+
+
+                    // Ben bir öğretmenim karşımda 10 tane öğrenci var, öğrenciler soru sorarlar ben de yanıtlarım. Bu aradaki iletişimi gerçekleştiren de bir uygulamadır.
+                    // İlgili öğrenciye mesaj gönderiyorum sistem aracılığıyla. Online cevap veren bir sistem.
     {
         static void Main(string[] args)
         {
+
+        }
+
+        abstract class CourseMember
+        {
+
+        }
+
+        class Teacher : CourseMember
+        {
+
+        }
+
+        class Student : CourseMember
+        {
+
+        }
+
+        class Mediator // Mediator işlemleri yapan ve yönlendiren kısımdır. Öğrenciden mediatore mediatorden öğretmene öğretmenden mediatore sonra cevap öğrenciye
+        {
+
         }
     }
 }
