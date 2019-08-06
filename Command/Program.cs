ae50482592537e954ea10c046875ab5c5734865d
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-08-06 22:29:35 +0300

    Command Design Pattern
    Second

--- Command/Program.cs
+++ Command/Program.cs
@@ -14,5 +14,40 @@ namespace Command // bir metin dosyasını açtığımızda ya da herhangibi edi
         }
     }
 
+    class Stock 
+    {
+        private string _name = "Laptop"; // field olduğu için alt tire
+        private int _quantity = 10;
 
+        public void Buy()
+        {
+            Console.WriteLine("Stock : {0}, {1} Bought!" , _name, _quantity);
+        }
+
+        public void Sell()
+        {
+            Console.WriteLine("Stock : {0}, {1} Sold!", _name, _quantity);
+        }
+    }
+
+    interface IOrder
+    {
+        void Execute();
+    }
+
+    class BuyStock : IOrder
+    {
+        public void Execute()
+        {
+            
+        }
+    }
+
+    class SellStock : IOrder
+    {
+        public void Execute()
+        {
+            
+        }
+    }
 }
