ee08ef66149c834e634a78a3795c9149df66637f
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-08-10 22:35:46 +0300

    Visitor Design Pattern
    Second

--- Visitor/Program.cs
+++ Visitor/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Visitor
+namespace Visitor // birbirine benzeyen ya da hiyerarşik nesnelerin aynı methodunun biri üzerinden diğerlerinin de çağırılması. bir şirketteki tüm hiyerarşiye ödeme ya da maaş arttırımı yapmak istiyoruz. 
+    // kişilere göre zam yapma 
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+        }
+
+        class OrganisationalStructure
+        {
+            private EmployeeBase _employee;
+
+            public OrganisationalStructure(EmployeeBase firstEmployee)
+            {
+                _employee = firstEmployee;
+            }
+
+            public void Accept(VisitorBase visitor)
+            {
+                EmployeeBase.Accept(visitor);
+            }
+        }
+
+        abstract class EmployeeBase
+        {
+            public abstract  void Accept(VisitorBase visitor)
+            
+        }
+
+        class VisitorBase
+        {
+
         }
     }
 }
