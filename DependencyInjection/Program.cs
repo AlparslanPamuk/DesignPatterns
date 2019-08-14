cdb35704eb3020f5059bec2a22c08b86372af590
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-08-14 16:41:59 +0300

    Dependency Injection
    First

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class Program // Projelerde çokça kullanılır. 
+                  // Yazılım geliştirme prensiblerinin D harfidir. bir nesnenin başka bir nesneye bağımlılığının olmaması gerektiğini söyler. (dependency)
+                  // Harfler arası geçiş yaparken çokça kullanırız. 
+                  // farklı ormler kullanırken geçişleri kolayca yapmak 
+                  // cross cutting concernlerin yine dependency I ile kullanımı yoğundur.
+                  // Genelikle bussiness kodu yazarken, bir bussiness kodunda farklı birini kullanırken kendi içinde bağımlılığı minimize etmeyi önerir.
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class ProductDataAccessLayer
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with entityFramework");
+        }
+    }
+
+    class ProductManager
+    {
+        public void Save()
+        {
+            //Busiiness Code
+
+            ProductDataAccessLayer productDataAccessLayer = new ProductDataAccessLayer();
+            productDataAccessLayer.Save();
+        }
+    }
+}
