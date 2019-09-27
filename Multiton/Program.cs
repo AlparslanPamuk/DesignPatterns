53680ed09a129a69e8e100936594917ad4f5d5dc
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-09-27 16:20:54 +0300

    Multiton Design Pattern
    First

---
+++ Multiton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiton
+{
+    class Program
+    {
+        static void Main(string[] args) // signetonda bir nesneden sadece bir tane uretildiği garanti edilirdi. burada ise belli şartlara   instance'le üretilip belli 
+            //10 parametre varsa her parametre için bir instance üretilir  
+        {
+
+        }
+    }
+
+    class Multiton
+    {
+
+    }
+}
