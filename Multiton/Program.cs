2c5133a42211a8a34410b962517e4213b8bc8805
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-09-27 16:38:01 +0300

    Multiton Design Pattern
    Third

--- Multiton/Program.cs
+++ Multiton/Program.cs
@@ -11,22 +11,33 @@ namespace Multiton
         static void Main(string[] args) // signetonda bir nesneden sadece bir tane uretildiği garanti edilirdi. burada ise belli şartlara   instance'le üretilip belli 
             //10 parametre varsa her parametre için bir instance üretilir  
         {
+            Camera camera1 = Camera.GetCamera("NIKON"); // nikonlar için ayrı canonlar için ayrı guid  verecek ve onları kullanacak 
+            Camera camera2 = Camera.GetCamera("NIKON");
+            Camera camera3 = Camera.GetCamera("CANON"); // canon gönderip kamera 3 e bir instance istedim 
+            Camera camera4 = Camera.GetCamera("CANON");
 
+            Console.WriteLine(camera1.Id);
+            Console.WriteLine(camera2.Id);
+            Console.WriteLine(camera3.Id);
+            Console.WriteLine(camera4.Id);
+
+            Console.ReadLine();
         }
     }
 
     class Camera
     {
-        static Dictionary<string, Camera> _cameras = new Dictionary<string, Camera>();     //birden fazla instance olacağı için bunları bir dictionary ile tutuyoruz 
+        static Dictionary<string, Camera> _cameras = new Dictionary<string, Camera>(); //burada bir instance tutan camera listasi tutan onu da brande göre tutan bir liste   
+        //birden fazla instance olacağı için bunları bir dictionary ile tutuyoruz 
         static object _lock = new object();
         public Guid Id { get; set; }
         private string brand;
         private Camera()
         {
-            Id = Guid.NewGuid();
+            Id = Guid.NewGuid(); // Hepsi için tekrar dönecek 
         }
 
-        public static Camera GetCamera(string brand)
+        public static Camera GetCamera(string brand) //Listenin içinde benim gönderdiği mmarka için bir instance yoksa o zaman listeye yeni bir tane oluştur onu da key gibi tut
         {
             lock (_lock)
             {
