446c63286d63b867cea79adfe4a3e0f5f2a60dd9
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-09-27 16:05:35 +0300

    Null Object Design Pattern
    Second

--- NullObject/Program.cs
+++ NullObject/Program.cs
@@ -14,8 +14,20 @@ namespace NullObject
         }
     }
 
-    class CustomerManager
+    class CustomerManager // bağımlılığı 0 lamak için böyle kullanıyoruz *Customer manageri newlediğimde bana bir tane logger ver demek aşası
     {
+        private Ilogger _logger;
+
+        public CustomerManager(Ilogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            _logger.Log();
+        }
 
     }
 
@@ -24,10 +36,20 @@ namespace NullObject
         void Log();
     }
 
-    class Log4NetLogger
+    class Log4NetLogger : Ilogger
     {
-
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4Net");
+        }
     }
 
+    class NLogLogger : Ilogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with NLogLogger");
+        }
+    }
 
 }
